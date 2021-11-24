3461fc74f968f0bcb79295dbecae11c766126b4a
Dev deRev <devderev@free.fr>
2021-11-24 22:19:30 +0100

    Travail sur la barre de navigation

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -27,7 +27,7 @@ class CategoryController extends AbstractController
 
         $form->handleRequest($request);
 
-        if ($form->isSubmitted()) {
+        if ($form->isSubmitted() && $form->isValid()) {
 
             $category->setSlug(strtolower($slugger->slug($category->getName())));
 
@@ -61,7 +61,7 @@ class CategoryController extends AbstractController
 
         $form->handleRequest($request);
 
-        if ($form->isSubmitted()) {
+        if ($form->isSubmitted() && $form->isValid()) {
 
             $category->setSlug(strtolower($slugger->slug($category->getName())));
 
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,10 +2,11 @@
 
 namespace App\Entity;
 
-use App\Repository\CategoryRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=CategoryRepository::class)
@@ -21,6 +22,8 @@ class Category
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message= "Le nom de la catégorie est obligatoire")
+     * @Assert\Length(min= 3, minMessage= "Le nom de la catégorie doit contenir au moins {{ limit }} caractères")
      */
     private $name;
 
@@ -49,7 +52,7 @@ class Category
         return $this->name;
     }
 
-    public function setName(string $name): self
+    public function setName(?string $name): self
     {
         $this->name = $name;
 
