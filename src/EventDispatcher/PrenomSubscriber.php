6fcf9fdcae9a3d7a073868421ead297dfa414050
Dev deRev <devderev@free.fr>
2022-01-18 22:04:35 +0400

    désactivation des dumps avant la session

--- src/EventDispatcher/PrenomSubscriber.php
+++ src/EventDispatcher/PrenomSubscriber.php
@@ -12,8 +12,8 @@ class PrenomSubscriber implements EventSubscriberInterface
     {
         return [
             'kernel.request' => 'addPrenomToAttributes',
-            'kernel.controller' => 'test1',
-            'kernel.response' => 'test2'
+            // 'kernel.controller' => 'test1',
+            // 'kernel.response' => 'test2'
         ];
     }
 
